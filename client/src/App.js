0ec110ed88cbeb2f6c65f8f5960650932cbb366c
eladshachar <elad.shachar@gmail.com>
2022-11-18 19:21:50 +0200

    removed the reloading of a page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import axios from 'axios'
 import Operations from './components/Operations/Operations';
-import Expences from './components/Expences/Expences';
+import Expenses from './components/Expenses/Expenses';
 import Balance from './components/Balance/Balance';
 import Transactions from './components/Transactions/Transactions';
 
@@ -27,19 +27,20 @@ function App() {
   const getAllTransactions = () => {
     axios.get('http://127.0.0.1:8000/transactions').then((res) => { 
     setTransactions(res.data)
-    res.data !== null ? setTransactions(res.data) : setTransactions([])
+    res.data !== Object ? setTransactions(res.data) : setTransactions([])
     })
   }
 
   const removeTransaction = id => {
-    axios.delete(`http://127.0.0.1:8000/transactions?id=${id}`).then(()=> {
-      window.location.reload(false)
+    axios.delete(`http://127.0.0.1:8000/transactions/${id}`).then(()=> {
+      getAllTransactions()
+      filterByCategory()
     })
   }
 
   const filterByCategory = category => {
     axios.get(`http://127.0.0.1:8000/transactions?category=${category}`).then((result)=> { 
-        result.data !== null ? setFilteredTransactions(result.data) : setFilteredTransactions([])
+        result.data !== Object ? setFilteredTransactions(result.data) : setFilteredTransactions([])
     })
   }
 
@@ -47,15 +48,18 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div id="main-links">
+        <div id="header">
           <Link to='/' className='link'>Home </Link>
           <Link to='/operations' className='link'>Operations </Link>
-          <Link to='/expences' className='link'>Expences </Link>
+          <Link to='/expenses' className='link'>Expenses </Link>
           <Balance transactions={transactions}/>
         </div>
-        <Route path="/" exact render={()=> <Transactions transactions={transactions} removeTransaction={removeTransaction}/>}/>
-        <Route path='/operations' exact render={()=> <Operations />}/>
-        <Route path='/expences' exact render={()=> <Expences filteredTransactions={filteredTransactions} filterByCategory={filterByCategory} removeTransaction={removeTransaction} updateCategory={updateCategory}/>}/>
+        <div id='main-container'>
+          <Route path="/" exact render={()=> <Transactions transactions={transactions} removeTransaction={removeTransaction}/>}/>
+          <Route path='/operations' exact render={()=> <Operations />}/>
+          <Route path='/expenses' exact render={()=> <Expenses filteredTransactions={filteredTransactions} filterByCategory={filterByCategory} removeTransaction={removeTransaction} updateCategory={updateCategory}/>}/>
+        </div>
+        
       </div>
     </Router>
 
